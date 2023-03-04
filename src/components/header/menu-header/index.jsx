6b4d48bfb9b184b22c3f0a809e16b97deb454c33
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-03-04 13:54:40 -0300

    Add componentes no menu header

--- src/components/header/menu-header/index.jsx
+++ src/components/header/menu-header/index.jsx
@@ -1,8 +1,26 @@
-import HeaderForm from '../header-form'
 import *as M from './menu-header'
 export default _ => {
     return (
         <M.Menu>
+            <div className="menu-barra">
+                <form action="">
+                    <input type="text" name ="nomeEst" id='name' placeholder='Nome do estabelicimento:' />
+                    <input type="text" name='endereco' id='endereco' placeholder='Endereço:' />
+                    <select name="turno" id="turno">
+                        <option value="turno">Turno:</option>
+                        <option value="M">Matutino</option>
+                        <option value="V">Vespertino</option>
+                        <option value="noturno">Noturno</option>
+                    </select>
+                    <select name="area" id="area">
+                        <option value="area">Area:</option>
+                        <option value="U">Urbana</option>
+                        <option value="R">Rural</option>
+                    </select>
+                    <input type="date"  />
+
+                </form>
+            </div>
             
         </M.Menu>
     )
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,5 +1,7 @@
 import Header from '../header/header-top-primary'
+import Menu from '../header/menu-header';
 export default _ => 
 <>
     <Header/>
+    <Menu/>
 </>
