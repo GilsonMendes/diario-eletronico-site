d1f1eb38ee2de111c1ac368852f943a193752cf2
GilsonMendes <38471977+GilsonMendes@users.noreply.github.com>
2023-03-05 21:56:20 -0300

    Add hucks dos inputs

--- src/components/pages/formulario-cad/index.jsx
+++ src/components/pages/formulario-cad/index.jsx
@@ -1,23 +1,34 @@
 import *as F from './form'
+import React, { useState } from 'react'
 export default props => {
+
+    const [values, setValues] = useState()
+    console.log(values);
+    const inputDate = (value) => {
+
+        setValues(prevVelue => ({
+            ...prevVelue,
+            [value.target.name]: value.target.value,
+        }));
+    }
     return (
         <F.Form>
             <form action="">
                 <div className='areaAluno'>
-                    <input type="text" placeholder='Nome:' className='inputNome' />
-                    <select name="sexo" id="sexo" className='selectSexo'>
+                    <input type="text" name='Nome' placeholder='Nome:' className='inputNome' onChange={inputDate} />
+                    <select name="Sexo" id="sexo" className='selectSexo' onChange={inputDate}>
                         <option value="sexo">Sexo:</option>
                         <option value="maculino">M</option>
                         <option value="feminino">F</option>
                     </select>
-                    <input type="date"  className='inputDate'/>
-                    <input type="number" max='100' placeholder='Idade:' className='inputIdade' />
+                    <input type="date" name='Data' className='inputDate' onChange={inputDate} />
+                    <input type="number" name='Idade' max='100' placeholder='Idade:' className='inputIdade' onChange={inputDate} />
                 </div>
-                
+
                 <div className='areaSituacao'>
-                    <input type="text" placeholder='Disciplina:' className='disciplina' />
-                    <input type="text" placeholder='Nota:' className='inputNota' />
-                    <select name="situacao" id="situacao" className='selectSituacao'>
+                    <input type="text" name='Disciplina' placeholder='Disciplina:' className='disciplina' onChange={inputDate} />
+                    <input type="text" name='Nota' placeholder='Nota:' className='inputNota' onChange={inputDate} />
+                    <select name="Situacao" id="situacao" className='selectSituacao' onChange={inputDate}>
                         <option value="situacao">Situação Final:</option>
                         <option value="aprovado">Aprovado</option>
                         <option value="reprovado">Reprovado</option>
