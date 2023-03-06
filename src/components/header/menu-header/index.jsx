f3abb0bc93ef3c466a03d3f32594cc506a88e410
GilsonMendes <38471977+GilsonMendes@users.noreply.github.com>
2023-03-06 13:13:30 -0300

    Add hooks do header menu.

--- src/components/header/menu-header/index.jsx
+++ src/components/header/menu-header/index.jsx
@@ -1,23 +1,36 @@
+import React, {useState} from 'react';
 import *as M from './menu-header'
 export default _ => {
+
+    const [valueHeader, setValueHeader] = useState()
+     console.log(valueHeader)
+    const headerValues =(value)=>{
+        setValueHeader(prevValues => ({
+
+            ...prevValues,
+            [value.target.name] : value.target.value
+
+        }))
+    }
+    
     return (
         <M.Menu>
             <div className="menu-barra">
                 <form action="">
-                    <input className='nomeEst' type="text" name ="nomeEst" id='name' placeholder='Estabelicimento:' />
-                    <input className='endereco' type="text" name='endereco' id='endereco' placeholder='Endereço:' />
-                    <select name="turno" id="turno">
-                        <option value="turno">Turno:</option>
-                        <option value="M">Matutino</option>
-                        <option value="V">Vespertino</option>
-                        <option value="noturno">Noturno</option>
+                    <input className='nomeEst' type="text" name ="Nome do Estabelicimento" id='name' placeholder='Estabelicimento:' onChange={headerValues} />
+                    <input className='endereco' type="text" name='Endereco' id='endereco' placeholder='Endereço:' onChange={headerValues} />
+                    <select name="Turno" id="turno" onChange={headerValues}>
+                        <option value="">Turno:</option>
+                        <option value="Matutino">Matutino</option>
+                        <option value="Vespertino">Vespertino</option>
+                        <option value="Noturno">Noturno</option>
                     </select>
-                    <select name="area" id="area">
-                        <option value="area">Area:</option>
-                        <option value="U">Urbana</option>
-                        <option value="R">Rural</option>
+                    <select name="Área" id="area"  onChange={headerValues}>
+                        <option value="">Area:</option>
+                        <option value="Urbana">Urbana</option>
+                        <option value="Rural">Rural</option>
                     </select>
-                    <input className='date-area' type="month"  />
+                    <input name='Ano' className='date-area' type="month"  onChange={headerValues} />
 
                 </form>
             </div>
