5fc076e17e0f5d44c474779253652e4f927bf0bf
GilsonMendes <38471977+GilsonMendes@users.noreply.github.com>
2023-03-06 21:31:34 -0300

    Add axios lib

--- src/components/pages/formulario-cad/index.jsx
+++ src/components/pages/formulario-cad/index.jsx
@@ -1,50 +1,72 @@
 import *as F from './form'
 import React, { useState } from 'react'
-export default props => {
+import axios from 'axios'
+export default
+    props => {
 
-    const [values, setValues] = useState()
-    console.log(values);
-    const inputDate = (value) => {
+        const [values, setValues] = useState()
+        console.log(values);
+        const inputDate = (value) => {
 
-        setValues(prevVelue => ({
-            ...prevVelue,
-            [value.target.name]: value.target.value,
-        }));
+            setValues(prevVelue => ({
+                ...prevVelue,
+                [value.target.name]: value.target.value,
+            }));
+        }
+        const salvarDados = () => {
+
+            axios.post('/', {
+
+            })
+                .then(function (response) {
+                    console.log(response);
+                })
+
+            /*
+                 //JSON
+                   const {data} = await axios.post('/user', document.querySelector('#my-form'), {
+                        headers: {
+                        'Content-Type': 'application/json'
+                        }
+                    })
+             */
+
+
+        }
+        return (
+            <F.Form>
+                <form >
+                    <div className='areaAluno'>
+                        <input type="text" name='nome' placeholder='Nome:' className='inputNome' onChange={inputDate} />
+                        <select name="genero" id="sexo" className='selectSexo' onChange={inputDate}>
+                            <option value="">Sexo:</option>
+                            <option value="maculino">Masculino</option>
+                            <option value="feminino">Feminino</option>
+                            <option value="nb">Não binário</option>
+                            <option value="ag">Agênero</option>
+                            <option value="gf">Gênero fluído</option>
+                            <option value="tv">Travesti</option>
+                            <option value="ts">Transexual</option>
+                            <option value="in">Intersexo</option>
+                            <option value="ot">Outro</option>
+                        </select>
+                        <input type="date" name='data' className='inputDate' onChange={inputDate} />
+                        <input type="number" name='idade' max='100' placeholder='Idade:' className='inputIdade' onChange={inputDate} />
+                    </div>
+
+                    <div className='areaSituacao'>
+                        <input type="text" name='disciplina' placeholder='Disciplina:' className='disciplina' onChange={inputDate} />
+                        <input type="text" name='nota' placeholder='Nota:' className='inputNota' onChange={inputDate} />
+                        <select name="Situacao" id="situacaoFinal" className='selectSituacao' onChange={inputDate}>
+                            <option value="">Situação Final:</option>
+                            <option value="aprovado">Aprovado</option>
+                            <option value="reprovado">Reprovado</option>
+                        </select>
+                        <button type="submit">Salvar</button>
+                        <button>Excluir</button>
+                        <button className='btnLimpar'>Limpar</button>
+                    </div>
+                </form>
+            </F.Form >
+        )
     }
-    return (
-        <F.Form>
-            <form >
-                <div className='areaAluno'>
-                    <input type="text" name='nome' placeholder='Nome:' className='inputNome' onChange={inputDate} />
-                    <select name="genero" id="sexo" className='selectSexo' onChange={inputDate}>
-                        <option value="">Sexo:</option>
-                        <option value="maculino">Masculino</option>
-                        <option value="feminino">Feminino</option>
-                        <option value="nb">Não binário</option>
-                        <option value="ag">Agênero</option>
-                        <option value="gf">Gênero fluído</option>
-                        <option value="tv">Travesti</option>
-                        <option value="ts">Transexual</option>
-                        <option value="in">Intersexo</option>
-                        <option value="ot">Outro</option>
-                    </select>
-                    <input type="date" name='data' className='inputDate' onChange={inputDate} />
-                    <input type="number" name='idade' max='100' placeholder='Idade:' className='inputIdade' onChange={inputDate} />
-                </div>
-
-                <div className='areaSituacao'>
-                    <input type="text" name='disciplina' placeholder='Disciplina:' className='disciplina' onChange={inputDate} />
-                    <input type="text" name='nota' placeholder='Nota:' className='inputNota' onChange={inputDate} />
-                    <select name="Situacao" id="situacaoFinal" className='selectSituacao' onChange={inputDate}>
-                        <option value="">Situação Final:</option>
-                        <option value="aprovado">Aprovado</option>
-                        <option value="reprovado">Reprovado</option>
-                    </select>
-                    <button type="submit">Salvar</button>
-                    <button>Excluir</button>
-                    <button className='btnLimpar'>Limpar</button>
-                </div>
-            </form>
-        </F.Form >
-    )
-}
