c127c47e54fb874b4510343a298af41d107a6605
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-03-12 17:16:32 -0300

    Add api dados

--- src/components/views/index.jsx
+++ src/components/views/index.jsx
@@ -3,31 +3,35 @@ import axios from 'axios'
 /* import Card from './cards/cardAlunos' */
 
 export default function alunos() {
-    const [viewTables, setViewTables] = useState([]);
-    const [idNumber, setIdNumber] = useState()
-    console.log(idNumber)
-    const inputDate = (value) => {
+    const [viewTables, setViewTables] = useState();
 
-        setIdNumber(value.target.value);
-    }
+     const json = JSON.stringify(viewTables)
+    /*  const [idNumber, setIdNumber] = useState()
+     console.log(idNumber)
+     const inputDate = (value) => {
+ 
+         setIdNumber(value.target.value);
+     } */
 
-    const pegarDados = async () => {
 
-        await axios.get('http://localhost:3000/cadastroAlunos/')
+
+    const pegarDados = () => {
+
+        axios.get('http://localhost:3000/cadastroAlunos/')
             .then(({ data }) => {
-                setViewTables(data[idNumber].nome)
+                setViewTables(data)
+               
             })
+
     }
-
-
 
     console.log(viewTables)
 
     return (
         <div>
-            <p>Nome:{viewTables}</p>
-            <span>Id: </span>
-            <input type="number"  name='ID' onChange={inputDate} />
+
+           
+
             <button onClick={pegarDados}>Consultar</button>
         </div>
 
