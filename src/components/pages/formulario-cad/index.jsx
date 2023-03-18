6fb72c8f03c16b0841b2fe2b323ea160b8b42985
Gilson Mendes <gilson.mendes00@hotmail.com>
2023-03-18 18:30:09 -0300

    Add novas validações no formulario

--- src/components/pages/formulario-cad/index.jsx
+++ src/components/pages/formulario-cad/index.jsx
@@ -9,6 +9,11 @@ import * as yup from "yup";
 
 
 const schema = yup.object({
+    nomeEstabelecimento: yup.string().required(msnEstab),
+    endereco: yup.string().required(msnEndereco),
+    turno: yup.string().required(msnTurno),
+    area: yup.string().required(msnArea),
+    ano:yup.string().required(msnAno),
     nome: yup.string().required(msnNome),
     genero: yup.string().required(msnGenero),
     dataDeNascimento: yup.string().required(msnData),
@@ -17,8 +22,12 @@ const schema = yup.object({
     situacaoFinal: yup.string().required(msnSFinal),
 
 }).required();
-
-function msnNome() { toast.error("Digite um nome para o aluno!") }
+function msnEstab() { toast.error("Digite o nome do estabelecimento!") }
+function msnEndereco() { toast.error("Digite um endereço!") }
+function msnTurno() { toast.error("Entre com um turno!") }
+function msnArea() { toast.error("Selecione a área!") }
+function msnAno() { toast.error("Entre com o ano atual!") }
+function msnNome() { toast.error("Digite o nome do aluno!") }
 function msnGenero() { toast.error("Selecione o genero do aluno!") }
 function msnData() { toast.error("Digite uma data de nascimento!") }
 function msnDisc() { toast.error("Digite a disciplina!") }
@@ -76,10 +85,10 @@ export default props => {
     return (
 
         <F.Form>
-            <form /* onSubmit={handleSubmit((data) => console.log(data))} */>
+            <form onSubmit={handleSubmit((data) => console.log(data))}>
                 <div className='form'>
 
-                    <div className="menu-barra">
+                    <div className="menu-barra"> {/**Inicio form header */}
 
                         <input {...register('nomeEstabelecimento', { required: true })}
                             {...errors.nomeEstabelecimento && errors.nomeEstabelecimento?.message}
@@ -90,21 +99,36 @@ export default props => {
                             id='name'
                             placeholder='Estabelicimento:'
                             onChange={inputDate} />
-                        <input className='endereco' type="text" name='endereco' id='endereco' placeholder='Endereço:' onChange={inputDate} />
-                        <select name="turno" id="turno" onChange={inputDate}>
+                        <input {...register('endereco', { required: true })}
+                            {...errors.endereco && errors.endereco?.message}
+                            className='endereco'
+                            type="text" name='endereco'
+                            id='endereco'
+                            placeholder='Endereço:'
+                            onChange={inputDate} />
+                        <select {...register('turno', { required: true })} name="turno" id="turno" onChange={inputDate}>
                             <option value="">Turno:</option>
                             <option value="M">Matutino</option>
                             <option value="V">Vespertino</option>
                             <option value="N">Noturno</option>
+                            {errors.turno && errors.turno?.message}
                         </select>
-                        <select name="area" id="area" onChange={inputDate}>
+                        <select {...register('area', { required: true })} name="area" id="area" onChange={inputDate}>
                             <option value="">Area:</option>
                             <option value="U">Urbana</option>
                             <option value="R">Rural</option>
+                            {errors.turno && errors.turno?.message}
                         </select>
-                        <input name='ano' className='date-area' type="number" placeholder='Ano:' onChange={inputDate} />
+                        <input {...register('ano', { required: true })}
+                            {...errors.area && errors.area?.message}
+                            name='ano'
+                            className='date-area'
+                            type="number"
+                            placeholder='Ano:'
+                            onChange={inputDate} />
 
                     </div>
+                    {/**Fim form header */}
                     <div className='areaAluno'>
                         <input
                             {...register('nome', { required: true })}
@@ -170,8 +194,6 @@ export default props => {
                         <button type="submit" onClick={
                             function (e) {
                                 /*  salvarDados() */
-                                props.salvar()
-
                             }
                         }>Salvar</button>
                         <button className='btnLimpar' onClick={limpar}>Limpar</button>
